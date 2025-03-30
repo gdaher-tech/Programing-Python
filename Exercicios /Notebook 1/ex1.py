8afd52ef8a08545fd222a8aa5834818667b7fe6e
gdaher-tech <gustavo2000daher@gmail.com>
2025-03-30 19:56:07 -0300

    Enviando primeira parte das atividades

---
+++ Exercicios /Notebook 1/ex1.py
@@ -0,0 +1,14 @@
+## Criação de ‘Strings’:
+## Crie uma ‘string’ que contenha o seu nome completo e imprima-a.
+## Crie uma ‘string’ que contenha uma citação famosa e imprima-a usando aspas duplas.
+
+meu_nome_completo = 'Gustavo Daher Alves Garcia'
+print(meu_nome_completo)
+
+citation_famosa = "Mark Zuckerberg uma vez disse 'O maior risco é não correr nenhum risco'"
+print(citation_famosa)
+
+
+
+
+
---
+++ Exercicios /Notebook 1/ex2.py
@@ -0,0 +1,7 @@
+
+## Manipulation de ‘strings’
+## Crie uma ‘string’ que contenha uma frase. Use Fatiamento para imprimir apenas a primeira palavra da frase.
+## Dada ‘string’ "Python é incrível", use o met replace para substituir "incrível" por "Fantástico".
+
+frase = '"A persistência é o caminho do êxito." - Charles Chaplin'
+print(frase.split(''))
